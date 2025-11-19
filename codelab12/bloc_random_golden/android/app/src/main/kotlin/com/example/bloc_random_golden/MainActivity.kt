b51ddcfc0141fff9cdbb46dcd64b5290ffd63902
Golden Christian <gotiano.pro@gmail.com>
2025-11-19 10:54:42 +0700

    commit

---
+++ codelab12/bloc_random_golden/android/app/src/main/kotlin/com/example/bloc_random_golden/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bloc_random_golden
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- photo_filter_carousel/lib/widget/carousel_flowdelegate.dart
+++ photo_filter_carousel/lib/widget/carousel_flowdelegate.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 import 'package:flutter/rendering.dart';
